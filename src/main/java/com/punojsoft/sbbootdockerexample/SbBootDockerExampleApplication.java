f92081ca6cc5c72e39d4f9645863335176a592bb
punojbista55@gmail.com <dipen55555>
2021-07-23 22:43:13 +0545

    Example project for dockerization of spring boot app!

---
+++ src/main/java/com/punojsoft/sbbootdockerexample/SbBootDockerExampleApplication.java
@@ -0,0 +1,20 @@
+package com.punojsoft.sbbootdockerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@RequestMapping
+public class SbBootDockerExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbBootDockerExampleApplication.class, args);
+    }
+
+    @GetMapping(value = "/")
+    public String index() {
+        return "Spring boot docker example!";
+    }
+}
---
+++ src/test/java/com/punojsoft/sbbootdockerexample/SbBootDockerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.punojsoft.sbbootdockerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbBootDockerExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
